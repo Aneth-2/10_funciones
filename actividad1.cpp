ff443771b4f1b067cb63ce2af7148ea77cad226b
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-06-30 22:00:00 -0500

    Añade actividad1.cpp

---
+++ actividad1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+#define M_PI 3.14159
+
+using namespace std;
+int main(){
+	
+    double radio, Area;
+    cout<<"Ingrese el valor del radio: ";
+    cin>>radio;
+   
+    return 0;
+}
+
