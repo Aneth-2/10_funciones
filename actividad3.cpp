1a0bdbbd0978e787a92d7c191334800c71cc3d36
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-06-30 22:12:34 -0500

    Añade actividad3.cpp

---
+++ actividad3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+    double c1, c2, hipotenusa;
+
+    cout<<"Ingrese el primer cateto: "; cin>>c1;
+    cout<<"Ingrese el segundo cateto: "; cin>>c2;
+
+    
+
+    cout<<"La hipotenusa es: "<<hipotenusa<<endl;
+
+}
+
