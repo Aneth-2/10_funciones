3dc7d8bc87c8a5036221456762919cd20e6433f5
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-06-30 22:11:22 -0500

    se añadio for

--- actividad2.cpp
+++ actividad2.cpp
@@ -6,8 +6,11 @@ int main() {
     int number,randomNumber;
     cout<<"Cuantos numeros quiere generar: ";
     cin>>number;
-
-   
+    
+	for (int i = 0; i < number; i++) {
+        randomNumber = rand() % 100;
+        cout<<randomNumber<<endl;
+    }  
     return 0;
 }
 
