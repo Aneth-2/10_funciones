5b1ec479a2303f3954cc28b937795c0b67210ce8
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-06-30 22:08:42 -0500

    Añade  actividad2.cpp

---
+++ actividad2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main() {
+    int number,randomNumber;
+    cout<<"Cuantos numeros quiere generar: ";
+    cin>>number;
+
+   
+    return 0;
+}
+
